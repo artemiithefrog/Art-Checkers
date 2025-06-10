0d58661447c2362018273c5f81c03dfb000b20af
Artemii Malyshev <115695294+artemiithefrog@users.noreply.github.com>
2025-06-10 11:57:51 +0200

    Initial Commit

---
+++ Art-Checkers/Art_CheckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_CheckersApp.swift
+//  Art-Checkers
+//
+//  Created by artemiithefrog on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Art_CheckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Art-Checkers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Art-Checkers
+//
+//  Created by artemiithefrog on 10.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
